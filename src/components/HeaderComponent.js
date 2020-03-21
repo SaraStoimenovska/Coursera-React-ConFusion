5c009dcadcfe86ca3093a4e4cb1e525944238cd1
Sara Stoimenovska <sara.stoimenovska@yahoo.com>
2020-03-21 18:32:42 +0100

    Intro to Redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -58,42 +58,45 @@ class Header extends Component {
   render() {
     return (
       <>
-        <Navbar dark expand="md">
-          <div className="container">
+        <Navbar dark expand='md'>
+          <div className='container'>
             <NavbarToggler onClick={this.toggleNav} />
-            <NavbarBrand className="mr-auto" href="/">
+            <NavbarBrand className='mr-auto' href='/'>
               <img
-                src="assets/images/logo.png"
-                height="30"
-                width="41"
-                alt="Ristorante Con Fusion"
+                src='assets/images/logo.png'
+                height='30'
+                width='41'
+                alt='Ristorante Con Fusion'
               />
             </NavbarBrand>
             <Collapse isOpen={this.state.isNavOpen} navbar>
               <Nav navbar>
                 <NavItem>
-                  <NavLink className="nav-link" to="/home">
-                    <span className="fa fa-home fa-lg"></span> Home
+                  <NavLink className='nav-link' to='/home'>
+                    <span className='fa fa-home fa-lg'></span> Home
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/aboutus">
-                    <span className="fa fa-info fa-lg"></span> About Us
+                  <NavLink className='nav-link' to='/aboutus'>
+                    <span className='fa fa-info fa-lg'></span> About Us
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/menu">
-                    <span className="fa fa-list fa-lg"></span> Menu
+                  <NavLink className='nav-link' to='/menu'>
+                    <span className='fa fa-list fa-lg'></span> Menu
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/contactus"></NavLink>
+                  <NavLink className='nav-link' to='/contactus'>
+                    <span className='fa fa-address-card fa-lg'></span> Contact
+                    Us
+                  </NavLink>
                 </NavItem>
               </Nav>
-              <Nav className="ml-auto" navbar>
-                <NavLink className="nav-link" to="/contactus">
+              <Nav className='ml-auto' navbar>
+                <NavLink className='nav-link' to='/contactus'>
                   <Button outline onClick={this.toggleModal}>
-                    <span className="fa fa-sign-in fa-lg"></span> Login
+                    <span className='fa fa-sign-in fa-lg'></span> Login
                   </Button>
                 </NavLink>
               </Nav>
@@ -101,9 +104,9 @@ class Header extends Component {
           </div>
         </Navbar>
         <Jumbotron>
-          <div className="container">
-            <div className="row row-header">
-              <div className="col-12 col-sm-6">
+          <div className='container'>
+            <div className='row row-header'>
+              <div className='col-12 col-sm-6'>
                 <h1>Ristorante Con Fusion</h1>
                 <p>
                   We take inspiration from World's best cuisines, and create a
@@ -119,35 +122,35 @@ class Header extends Component {
           <ModalBody>
             <Form onSubmit={this.handleLogin}>
               <FormGroup>
-                <Label htmlFor="username">Username</Label>
+                <Label htmlFor='username'>Username</Label>
                 <Input
-                  type="text"
-                  id="username"
-                  name="username"
+                  type='text'
+                  id='username'
+                  name='username'
                   innerRef={input => (this.username = input)}
                 />
               </FormGroup>
               <FormGroup>
-                <Label htmlFor="password">Password</Label>
+                <Label htmlFor='password'>Password</Label>
                 <Input
-                  type="password"
-                  id="password"
-                  name="password"
+                  type='password'
+                  id='password'
+                  name='password'
                   innerRef={input => (this.password = input)}
                 />
               </FormGroup>
               <FormGroup check>
                 <Label check>
                   <Input
-                    type="checkbox"
-                    id="rememberme"
-                    name="rememberme"
+                    type='checkbox'
+                    id='rememberme'
+                    name='rememberme'
                     innerRef={input => (this.rememberme = input)}
                   />
                   Remember me
                 </Label>
               </FormGroup>
-              <Button type="submit" value="submit" color="primary">
+              <Button type='submit' value='submit' color='primary'>
                 Login
               </Button>
             </Form>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
+
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { Switch, Redirect, Route } from 'react-router-dom';
+import { Switch, Redirect, Route, withRouter } from 'react-router-dom';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
 import About from './AboutComponent';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  };
+};
 
 class Main extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS
-    };
   }
 
   render() {
     const HomePage = () => {
       return (
         <Home
-          dish={this.state.dishes.filter(dish => dish.featured)[0]}
+          dish={this.props.dishes.filter(dish => dish.featured)[0]}
           promotion={
-            this.state.promotions.filter(promotion => promotion.featured)[0]
+            this.props.promotions.filter(promotion => promotion.featured)[0]
           }
-          leader={this.state.leaders.filter(leader => leader.featured)[0]}
+          leader={this.props.leaders.filter(leader => leader.featured)[0]}
         />
       );
     };
@@ -41,9 +41,9 @@ class Main extends Component {
       return (
         <DishDetail
           dish={
-            this.state.dishes.filter(dish => dish.id === +match.params.id)[0]
+            this.props.dishes.filter(dish => dish.id === +match.params.id)[0]
           }
-          comments={this.state.comments.filter(
+          comments={this.props.comments.filter(
             comment => comment.dishId === +match.params.id
           )}
         />
@@ -54,20 +54,20 @@ class Main extends Component {
       <div>
         <Header />
         <Switch>
-          <Route path="/home" component={HomePage} />
+          <Route path='/home' component={HomePage} />
           <Route
             exact
-            path="/menu"
-            component={() => <Menu dishes={this.state.dishes} />}
+            path='/menu'
+            component={() => <Menu dishes={this.props.dishes} />}
           />
-          <Route path="/menu/:id" component={DishWithId} />} />
-          <Route exact path="/contactus" component={Contact} />
+          <Route path='/menu/:id' component={DishWithId} />} />
+          <Route exact path='/contactus' component={Contact} />
           <Route
             exact
-            path="/aboutus"
-            component={() => <About leaders={this.state.leaders} />}
+            path='/aboutus'
+            component={() => <About leaders={this.props.leaders} />}
           />
-          <Redirect to="/home" />
+          <Redirect to='/home' />
         </Switch>
         <Footer />
       </div>
@@ -75,4 +75,4 @@ class Main extends Component {
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
